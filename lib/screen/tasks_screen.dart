d748514115e8a9768fcde22deefb63e8e2eb13fb
guopeng li <guopeng.li@technicolor.com>
2021-02-17 09:13:13 +0100

    Bottom show widget

--- lib/screen/tasks_screen.dart
+++ lib/screen/tasks_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screen/add_task_screen.dart';
+import 'package:todoey_flutter/widget/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
@@ -6,7 +8,20 @@ class TasksScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.lightBlueAccent, child: Icon(Icons.add)),
+          backgroundColor: Colors.lightBlueAccent,
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) => SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            bottom: MediaQuery.of(context).viewInsets.bottom),
+                        child: AddTaskScreen(),
+                      ),
+                    ));
+          },
+          child: Icon(Icons.add)),
       body: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -42,11 +57,13 @@ class TasksScreen extends StatelessWidget {
             ),
             Expanded(
               child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(20.0),
                         topRight: Radius.circular(20.0))),
+                child: TasksList(),
               ),
             )
           ],
