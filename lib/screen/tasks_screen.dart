472267b93027c204bacdd0ecbc4dc9f661f2cb18
guopeng li <guopeng.li@technicolor.com>
2021-03-05 14:11:01 +0100

    using callback to lift up state

--- lib/screen/tasks_screen.dart
+++ lib/screen/tasks_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/model/task.dart';
 import 'package:todoey_flutter/screen/add_task_screen.dart';
 import 'package:todoey_flutter/widget/tasks_list.dart';
 
-class TasksScreen extends StatelessWidget {
+class TasksScreen extends StatefulWidget {
+  @override
+  _TasksScreenState createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +29,12 @@ class TasksScreen extends StatelessWidget {
                       child: Container(
                         padding: EdgeInsets.only(
                             bottom: MediaQuery.of(context).viewInsets.bottom),
-                        child: AddTaskScreen(),
+                        child: AddTaskScreen((newTaskTitle) {
+                          setState(() {
+                            tasks.add(Task(name: newTaskTitle));
+                          });
+                          Navigator.pop(context);
+                        }),
                       ),
                     ));
           },
@@ -49,7 +66,7 @@ class TasksScreen extends StatelessWidget {
                         fontWeight: FontWeight.w700),
                   ),
                   Text(
-                    '12 Tasks',
+                    '${tasks.length} Tasks',
                     style: TextStyle(color: Colors.white, fontSize: 18),
                   ),
                 ],
@@ -63,7 +80,7 @@ class TasksScreen extends StatelessWidget {
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(20.0),
                         topRight: Radius.circular(20.0))),
-                child: TasksList(),
+                child: TasksList(tasks),
               ),
             )
           ],
--- lib/widget/tasks_list.dart
+++ lib/widget/tasks_list.dart
@@ -3,32 +3,30 @@ import 'package:todoey_flutter/model/task.dart';
 import 'package:todoey_flutter/widget/task_tile.dart';
 
 class TasksList extends StatefulWidget {
+  final List<Task> tasks;
+
+  TasksList(this.tasks);
+
   @override
   _TasksListState createState() => _TasksListState();
 }
 
 class _TasksListState extends State<TasksList> {
-  List<Task> tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return TaskTile(
-          taskTitle: tasks[index].name,
-          isChecked: tasks[index].isDone,
+          taskTitle: widget.tasks[index].name,
+          isChecked: widget.tasks[index].isDone,
           checkboxCallback: (bool checkboxState) {
             setState(() {
-              tasks[index].toggleDone();
+              widget.tasks[index].toggleDone();
             });
           },
         );
       },
-      itemCount: tasks.length,
+      itemCount: widget.tasks.length,
     );
   }
 }
